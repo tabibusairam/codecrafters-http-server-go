4fe7899cf4ea967073252df799e89204fe5f0df2
Sairam Tabibu <sairamtabibu@Sairams-MacBook-Air.local>
2024-03-30 01:37:41 -0700

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
+	var response = "HTTP/1.1 200 OK\r\n\r\n"
 	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -19,9 +20,14 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+	conn, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	
+	fmt.Println("debug1")
+1
+	net.Conn.Write(conn, []byte(response))
+	fmt.Println("debug2")
 }
