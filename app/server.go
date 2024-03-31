d2be722ee3ab1ac77d530ca2abfbf9c54c570b45
Sairam Tabibu <sairamtabibu@Sairams-MacBook-Air.local>
2024-03-31 00:41:40 -0700

    change8

--- app/server.go
+++ app/server.go
@@ -1,5 +1,6 @@
 package main
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -13,6 +14,7 @@ import (
 const (
 	OkResponse                        = "HTTP/1.1 200 OK\r\n"
 	NotFoundResponse                  = "HTTP/1.1 404 Not Found\r\n"
+	StatusCreated                     = "HTTP/1.1 201 Created\r\n"
 	ContentTypeTextPlain              = "Content-Type: text/plain\r\n"
 	ContentTypeApplicationOctetStream = "Content-Type: application/octet-stream\r\n"
 )
@@ -55,33 +57,55 @@ func connHandler(conn net.Conn, dir string) {
 	}
 	lines := strings.Split(string(receivedBytes), "\r\n")
 	path := strings.Split(lines[0], " ")[1]
-	switch {
-	case path == "/":
-		_, err = conn.Write([]byte(OkResponse + "\r\n"))
-	case strings.Contains(path, "/echo/"):
-		_, word, _ := strings.Cut(path, "/echo/")
-		fmt.Println("Received ECHO for word: ", word, "length: ", strconv.Itoa(len(word)))
-		_, err = conn.Write([]byte((OkResponse + ContentTypeTextPlain + contentLength(word) + word + "\r\n")))
-	case strings.Contains(path, "/user-agent"):
-		for _, line := range lines {
-			if strings.Contains(line, "User-Agent") {
-				userAgent := strings.Split(line, "User-Agent: ")[1]
-				_, err = conn.Write([]byte((OkResponse + ContentTypeTextPlain + contentLength(userAgent) + userAgent + "\r\n")))
+	requestType := strings.Split(lines[0], " ")[0]
+		log.Println("Request type: ", requestType)
+		switch {
+		case path == "/":
+			_, err = conn.Write([]byte(OkResponse + contentLength("") + "\r\n"))
+		case strings.Contains(path, "/echo/"):
+			_, word, _ := strings.Cut(path, "/echo/")
+			fmt.Println("Received ECHO for word: ", word, "length: ", strconv.Itoa(len(word)))
+			_, err = conn.Write([]byte((OkResponse + ContentTypeTextPlain + contentLength(word) + word + "\r\n")))
+		case strings.Contains(path, "/user-agent"):
+			for _, line := range lines {
+				if strings.Contains(line, "User-Agent") {
+					userAgent := strings.Split(line, "User-Agent: ")[1]
+					_, err = conn.Write([]byte((OkResponse + ContentTypeTextPlain + contentLength(userAgent) + userAgent + "\r\n")))
+				}
 			}
-		}
-	case strings.Contains(path, "/files/"):
-		_, file, _ := strings.Cut(path, "/files/")
-		fmt.Println("Received GET FILES for file: ", file, "length: ", strconv.Itoa(len(file)), dir+file)
-		fileContent, err := os.ReadFile(dir + file)
-		if err != nil {
-			log.Println(err)
-			fmt.Println("File not found")
+		case strings.Contains(path, "/files/"):
+			if requestType != "POST" {
+				_, file, _ := strings.Cut(path, "/files/")
+				fmt.Println("Received GET FILES for file: ", file, "length: ", strconv.Itoa(len(file)), dir+file)
+				fileContent, err := os.ReadFile(dir + file)
+				if err != nil {
+					log.Println(err)
+					fmt.Println("File not found")
+					_, err = conn.Write([]byte(NotFoundResponse + contentLength("") + "\r\n"))
+				} else {
+					fmt.Println("File found")
+					outfileContent := string(fileContent)
+					outfileContent = strings.Replace(outfileContent, "\x00", "", -1)
+					log.Println(outfileContent)
+					craftedResponse := []byte(OkResponse + ContentTypeApplicationOctetStream + contentLength(outfileContent) + outfileContent + "\r\n")
+					_, err = conn.Write(bytes.Trim(craftedResponse, "\x00"))
+				}
+			} else {			log.Println(lines[len(lines)-1])
+				log.Println("File creation")
+				_, file, _ := strings.Cut(path, "/files/")
+				log.Println(dir + file)
+				f, _ := os.Create(dir + file)
+				inputFileContent := strings.Replace(lines[len(lines)-1], "\x00", "", -1)
+				_, err2 := f.WriteString(inputFileContent)
+				if err2 != nil {
+					return
+				}
+				_, err = conn.Write([]byte(StatusCreated + contentLength("") + "\r\n"))
+			}
+		default:
 			_, err = conn.Write([]byte(NotFoundResponse + contentLength("") + "\r\n"))
-		} else {
-			fmt.Println("File found")
-			_, err = conn.Write([]byte((OkResponse + ContentTypeApplicationOctetStream + contentLength(string(fileContent)) + string(fileContent) + "\r\n")))
-		}
-	default:		_, err = conn.Write([]byte(NotFoundResponse + contentLength("") + "\r\n"))
 			fmt.Println("Received GET for ", path)
 		}
 	}
+	
+	
