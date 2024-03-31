2e56f2e5e971a297f37d85b962a2d6053569ec5c
Sairam Tabibu <sairamtabibu@Sairams-MacBook-Air.local>
2024-03-31 00:37:15 -0700

    change7

--- app/server.go
+++ app/server.go
@@ -1,86 +1,87 @@
-
-
 package main
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
-)
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 	// Uncomment this block to pass the first stage
-	//
-	listener, err := net.Listen("tcp", "0.0.0.0:4221")
+	// "net"
+	// "os"
+)
+const (
+	OkResponse                        = "HTTP/1.1 200 OK\r\n"
+	NotFoundResponse                  = "HTTP/1.1 404 Not Found\r\n"
+	ContentTypeTextPlain              = "Content-Type: text/plain\r\n"
+	ContentTypeApplicationOctetStream = "Content-Type: application/octet-stream\r\n"
+)
+func contentLength(str string) string {
+	length := len(str)
+	return "Content-Length: " + strconv.Itoa(length) + "\r\n\r\n"
+}
+func main() {
+	defaultDirectory := "./"
+	args := os.Args
+	if len(args) > 2 {
+		if args[1] == "--directory" {
+			defaultDirectory = args[2]
+		}
+	}
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	fmt.Println("Server up listening on 0.0.0.0:4221")
 	for {
-		conn, err := listener.Accept()
+		// Waiting for connection
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		fmt.Println("Accepted connection")
+		go connHandler(conn, defaultDirectory)
 	}
 }
-func handleConnection(connection net.Conn) {
-
-	defer connection.Close()
-	buffer := make([]byte, 1024)
-	connection.Read(buffer)
-	request := string(buffer)
-	firstLine := request[:strings.Index(request, "\n")]
-	path := firstLine[strings.Index(firstLine, "/") : strings.LastIndex(firstLine, "HTTP")-1]
-	fmt.Println("Path: ", path)
-	if path == "/" {
-		response := "HTTP/1.1 200 OK\r\n\r\n"
-		_, err := connection.Write([]byte(response))
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-			fmt.Println("Failed to response", err.Error())
-		}
-		connection.Close()
-	} else if strings.HasPrefix(path, "/echo") {
-		echoParam := path[6:]
-		body := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(echoParam)) + "\r\n\r\n" + echoParam
-		_, err := connection.Write([]byte(body))
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-			fmt.Println("Failed to response", err.Error())
-		}
-		connection.Close()
-	} else if strings.HasPrefix(path, "/user-agent") {
-		requestLines := strings.Split(request, "\n")
-		userAgent := ""
-		for _, line := range requestLines {
-			if strings.HasPrefix(line, "User-Agent") {
-				userAgent = line[strings.Index(line, ":")+2 : len(line)-1]
-				break
+// Manage each connection
+func connHandler(conn net.Conn, dir string) {
+	receivedBytes := make([]byte, 1024)
+	_, err := conn.Read(receivedBytes)
+	if err != nil {
+		fmt.Println("Error while reading data")
+		os.Exit(1)
+	}
+	lines := strings.Split(string(receivedBytes), "\r\n")
+	path := strings.Split(lines[0], " ")[1]
+	switch {
+	case path == "/":
+		_, err = conn.Write([]byte(OkResponse + "\r\n"))
+	case strings.Contains(path, "/echo/"):
+		_, word, _ := strings.Cut(path, "/echo/")
+		fmt.Println("Received ECHO for word: ", word, "length: ", strconv.Itoa(len(word)))
+		_, err = conn.Write([]byte((OkResponse + ContentTypeTextPlain + contentLength(word) + word + "\r\n")))
+	case strings.Contains(path, "/user-agent"):
+		for _, line := range lines {
+			if strings.Contains(line, "User-Agent") {
+				userAgent := strings.Split(line, "User-Agent: ")[1]
+				_, err = conn.Write([]byte((OkResponse + ContentTypeTextPlain + contentLength(userAgent) + userAgent + "\r\n")))
 			}
 		}
-		body := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(userAgent)) + "\r\n\r\n" + userAgent
-		_, err := connection.Write([]byte(body))
+	case strings.Contains(path, "/files/"):
+		_, file, _ := strings.Cut(path, "/files/")
+		fmt.Println("Received GET FILES for file: ", file, "length: ", strconv.Itoa(len(file)), dir+file)
+		fileContent, err := os.ReadFile(dir + file)
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-			fmt.Println("Failed to response", err.Error())
+			log.Println(err)
+			fmt.Println("File not found")
+			_, err = conn.Write([]byte(NotFoundResponse + contentLength("") + "\r\n"))
+		} else {
+			fmt.Println("File found")
+			_, err = conn.Write([]byte((OkResponse + ContentTypeApplicationOctetStream + contentLength(string(fileContent)) + string(fileContent) + "\r\n")))
 		}
-		connection.Close()
-	} else {
-		response := "HTTP/1.1 404 Not Found\r\n\r\n"
-		_, err := connection.Write([]byte(response))
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-			fmt.Println("Failed to response", err.Error())
+	default:		_, err = conn.Write([]byte(NotFoundResponse + contentLength("") + "\r\n"))
+			fmt.Println("Received GET for ", path)
 		}
-		connection.Close()
 	}
-}
-
